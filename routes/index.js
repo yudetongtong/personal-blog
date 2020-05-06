d6dad6c094954b3f1255972803ed2dccd98936e6
ROLYPOLY111 <huangjie1234huang@qq.com>
2020-05-06 20:30:34 +0800

    新增文章发表与分页功能

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,39 @@
 var express = require('express');
 var router = express.Router();
 const model = require('../model')
+const moment = require('moment')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',function(req, res, next) {
   let username = req.session.username
-  res.render('index', { username:username,title:"首页" });
+  //当前页数
+  let page = req.query.page || 1 
+  let totalData = {
+    total:0,//文章总共有多少页
+    nowPage:page,//当前页
+    list:[]//当前页的文章列表
+  }
+
+  let pageSize = 2 ;//每页请求两条数据
+
+  model.connectDb((db)=>{
+    //第一步，查询所有文章
+    db.collection('articles').find().toArray((err, ret) => {
+      console.log('文章列表', err)
+      
+      totalData.total = Math.ceil(ret.length / pageSize) 
+      //第二步，查询当前页的文章列表
+      model.connectDb((db)=>{
+        db.collection('articles').find().sort({_id:-1}).limit(pageSize).skip((page-1)*pageSize).toArray((err,ret2)=>{
+          ret2.map((item, index) => {
+            item['time'] = moment(item.timeId).format('YYYY-MM-DD HH:mm:ss')
+          })
+          totalData.list = ret2
+          res.render('index', { username, title: "首页", totalData });
+        })
+      })
+    })
+  })
 });
 
 //渲染注册页
@@ -20,7 +48,7 @@ router.get('/login',(req,res,next)=>{
 
 //渲染写文章页面
 router.get('/write',(req,res,next)=>{
-  let username = req.session.username || null
+  let username = req.session.username
   res.render('write', { username: username, title: "写文章" })
 })
 
